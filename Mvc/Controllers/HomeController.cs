a45b71dd24253ee39ebc572c376bfb887dc5c684
deltagare <deltagare@LEXSMAAF12>
2017-02-22 15:31:52 +0100

    Add project files.

---
+++ Mvc/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About() // the first container 
+        {
+            return View();
+        }
+
+        public ActionResult Contact() // the second container 
+        {
+            return View();
+        }
+
+        public ActionResult Project() // the first container 
+        {
+            return View();
+        }
+
+
+
+    }
+}
